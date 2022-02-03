54f61949ab0230ce0f7e2e589520f097476a1d76
apurvazawar <zawar.ap@northeastern.edu>
2022-02-02 19:45:41 -0500

    Create Panel and View Panel

---
+++ Lab_1/src/model/Car.java
@@ -0,0 +1,157 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author apurvazawar
+ */
+public class Car {
+    
+    private String brand;
+    private String model;
+    private String color;
+    private int year;
+    private String engineNo;
+    private int seatsNo;
+    private String licensePlate;
+    private String oName;
+    private int oTel;
+    private String oEmail;
+    private int oLicense;
+    private int oSSN;
+    private String oAddress;
+    private String serviceRecords;
+    private int warrantyYear;
+    private String photo;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getEngineNo() {
+        return engineNo;
+    }
+
+    public void setEngineNo(String engineNo) {
+        this.engineNo = engineNo;
+    }
+
+    public int getSeatsNo() {
+        return seatsNo;
+    }
+
+    public void setSeatsNo(int seatsNo) {
+        this.seatsNo = seatsNo;
+    }
+
+    public String getLicensePlates() {
+        return licensePlate;
+    }
+
+    public void setLicensePlates(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getoName() {
+        return oName;
+    }
+
+    public void setoName(String oName) {
+        this.oName = oName;
+    }
+
+    public int getoTel() {
+        return oTel;
+    }
+
+    public void setoTel(int oTel) {
+        this.oTel = oTel;
+    }
+
+    public String getoEmail() {
+        return oEmail;
+    }
+
+    public void setoEmail(String oEmail) {
+        this.oEmail = oEmail;
+    }
+
+    public int getoLicense() {
+        return oLicense;
+    }
+
+    public void setoLicense(int oLicense) {
+        this.oLicense = oLicense;
+    }
+
+    public int getoSSN() {
+        return oSSN;
+    }
+
+    public void setoSSN(int oSSN) {
+        this.oSSN = oSSN;
+    }
+
+    public String getoAddress() {
+        return oAddress;
+    }
+
+    public void setoAddress(String oAddress) {
+        this.oAddress = oAddress;
+    }
+
+    public String getServiceRecords() {
+        return serviceRecords;
+    }
+
+    public void setServiceRecords(String serviceRecords) {
+        this.serviceRecords = serviceRecords;
+    }
+
+    public int getWarrantyYear() {
+        return warrantyYear;
+    }
+
+    public void setWarrantyYear(int warrantyYear) {
+        this.warrantyYear = warrantyYear;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
